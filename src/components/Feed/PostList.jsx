d58c59137a121da8ac6ed624b334624022b2d900
kevin <kevinmpz10@icloud.com>
2025-11-24 11:57:46 -0300

    :sparkles: Render posts list with real backend data

--- src/components/Feed/PostList.jsx
+++ src/components/Feed/PostList.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import PostCard from "../Post/PostCard"
+
+
 
 const PostList = ({ posts }) => {
     return (
-        <div>PostList</div>
+        <div>
+            {posts.posts.map((posts) => (
+                <li key={posts.post_id}>
+                    <PostCard >
+                        {posts.user_id}
+                        {posts.content}
+                        {posts.image_url}
+                        {posts.created_at}
+                    </PostCard>
+                </li>))}
+        </div>
     )
 }
 
